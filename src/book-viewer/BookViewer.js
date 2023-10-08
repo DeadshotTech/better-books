5f5437f13cbbed7c7114eecf3c8e3e7b80caae72
Deadshot_Tech <vishalgautam.tech@gmail.com>
2023-10-08 16:38:42 +0530

    Added video viewer for book helper content

--- src/book-viewer/BookViewer.js
+++ src/book-viewer/BookViewer.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Modal from "react-modal";
 
 import Footer from "../footer/Footer";
 import NavBar from "../nav-bar/NavBar";
@@ -12,10 +13,12 @@ import PageHelperContent from "./PageHelperContent";
 export default function BookViewer({ book }) {
   const [currentPage, setCurrentPage] = useState(0);
   const [pages, setPages] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
 
   useEffect(() => {
     // Split text into pages
-    const charsPerPage = 1000; // Maximum characters per page
+    const charsPerPage = 3500; // Maximum characters per page
     const newPages = [];
     let remainingText = bookText;
     while (remainingText.length > 0) {
@@ -33,6 +36,19 @@ export default function BookViewer({ book }) {
 
   if (!book) return;
 
+  const openModal = () => {
+    setModalIsOpen(true);
+    setIsVideoLoaded(false); // Reset the video loaded state each time modal opens
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleVideoLoad = () => {
+    setIsVideoLoaded(true);
+  };
+
   const goToNextPage = () => {
     if (currentPage < pages.length - 1) {
       setCurrentPage(currentPage + 1);
@@ -43,6 +59,10 @@ export default function BookViewer({ book }) {
     if (currentPage > 0) {
       setCurrentPage(currentPage - 1);
     }
+  };
+
+  const handleHelperContentClick = () => {
+    openModal();
   };
 
   return (
@@ -60,12 +80,60 @@ export default function BookViewer({ book }) {
         <div className="page">
           <PageHelperContent
             helperContent={bookData?.pages[currentPage]?.helperContent?.left}
+            handleHelperContentClick={handleHelperContentClick}
           />
           <div className="page-content">
             <p>{pages[currentPage]}</p>
           </div>
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Video Modal"
+            ariaHideApp={false}
+            style={{
+              overlay: {
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+              },
+              content: {
+                width: "560px",
+                height: "365px",
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                bottom: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                padding: "20px",
+              },
+            }}
+          >
+            <div className="helper-content-modal">
+              <div className="helper-content-title">
+                <span>
+                  <strong>Video title should appear here</strong>
+                </span>
+                <button onClick={closeModal} className="close-button">
+                  Close ❌
+                </button>
+              </div>
+              <div className="videoContainer">
+                {!isVideoLoaded && <div className="loadingSpinner"></div>}
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/af4oGaPPuD8"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  onLoad={handleVideoLoad}
+                  style={{ display: isVideoLoaded ? "block" : "none" }}
+                />
+              </div>
+            </div>
+          </Modal>
           <PageHelperContent
             helperContent={bookData?.pages[currentPage]?.helperContent?.right}
+            handleHelperContentClick={handleHelperContentClick}
           />
         </div>
         <BookPaginator
--- src/book-viewer/PageHelperContent.js
+++ src/book-viewer/PageHelperContent.js
@@ -1,9 +1,16 @@
-export default function PageHelperContent({ helperContent }) {
+export default function PageHelperContent({
+  helperContent,
+  handleHelperContentClick,
+}) {
   return (
     <div className="page-images">
       {helperContent?.map((content, index) => {
         return (
-          <div className="image-container" key={index}>
+          <div
+            className="image-container"
+            key={index}
+            onClick={handleHelperContentClick}
+          >
             <img src={content.imageUrl} alt={content.imageUrl} />
             <p>{content.description}</p>
           </div>
